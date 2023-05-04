96b8c8efc38addab6c577b2d0bb1486a2d93d257
mak-somniferum <suhjin700@gmail.com>
2023-05-04 19:04:16 +0900

    day 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50%;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div style={{ display: "flex" }}>
+    //   <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
+    //   <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
+    // </div>
+
+    <Father>
+      <Box bgColor="teal">
+        <Text>Hello</Text>
+      </Box>
+      <Circle bgColor="tomato" />
+    </Father>
   );
 }
 
