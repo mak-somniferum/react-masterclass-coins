25eb2d7e11a3b7c8ac572962070eca04597e8bfd
mak-somniferum <suhjin700@gmail.com>
2023-05-09 08:30:55 +0900

    typescript - interface, optional props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Circle from "./Circle";
+
 function App() {
-  return <div></div>;
+  // interface가 정의된 컴포넌트에 반드시 보내야하는 prop이 들어가지 않으면 에러 뜸.
+  return (
+    <div>
+      <Circle borderColor="black" bgColor="teal" />
+      <Circle text="im here" bgColor="tomato" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Circle.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+  // interface: object shape을 typescript 에게 설명해준다.
+  bgColor: string; // required
+  borderColor: string; // optional
+}
+
+const Container = styled.div<ContainerProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
+`;
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
+}
+
+export default Circle;
