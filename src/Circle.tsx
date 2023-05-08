23156ed0cd6829f8d8ad5acf291dd7d79135e750
mak-somniferum <suhjin700@gmail.com>
2023-05-09 08:53:50 +0900

    typescript - useState

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface ContainerProps {
@@ -24,6 +25,10 @@ interface CircleProps {
 }
 
 function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  const [value, setValue] = useState<number | string>(1); // useState에서 초기값을 지정하면 타입(<>)을 지정하지 않아도 초기값의 데이터 타입으로 앞으로의 값의 타입까지 유추한다. 초기값을 지정하지 않으면 undefined.
+  setValue(3);
+  setValue("hello");
+  // setValue(true);
   return (
     <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
       {text}
