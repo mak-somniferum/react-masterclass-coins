13331aebc5c33ecaeadb969b7b668c1d425251c8
mak-somniferum <suhjin700@gmail.com>
2023-05-08 16:50:48 +0900

    styled-components themes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,22 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotateAnimate = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  display: flex;
   justify-content: center;
   align-items: center;
-  width: 200px;
-  height: 200px;
-  background-color: tomato;
-  animation: ${rotateAnimate} 1s linear infinite;
-
-  ${Emoji} {
-    &:hover {
-      font-size: 70px;
-    }
-
-    &:active {
-      opacity: 0.4;
-    }
-  }
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😀</Emoji>
-      </Box>
-      <Emoji>😀</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
