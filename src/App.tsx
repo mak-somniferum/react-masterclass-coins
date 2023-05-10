fb95a4c17c8c97e9f8f20c9ca39eb5a9fac08d84
mak-somniferum <suhjin700@gmail.com>
2023-05-10 14:20:13 +0900

    typescript - 복습

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 const Container = styled.div`
   background-color: ${(props) => props.theme.bgColor};
 `;
-const H1 = styled.h1`
+
+const H1 = styled.h1<{ active?: boolean }>`
   color: ${(props) => props.theme.textColor};
+  display: ${(props) => (props.active ? "block" : "none")};
 `;
 
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1 active={active}>{text}</H1>;
+}
+
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget);
+    setIsActive(!isActive);
+  };
+
   return (
     <Container>
-      <H1>Title</H1>
+      <Dummy text="Hello" active />
+      <Dummy text="Boom!" active={isActive} />
+      <button onClick={onClick}>Click me</button>
     </Container>
   );
 }
